06f367f06f011e06713ce76fb022c1086ea1cbc7
nmarceloar <nmarcelo.ar@gmail.com>
2018-10-27 00:05:05 -0300

    elimino archivos innecesarios

--- 3.Codigo/backend-test/server/server.js
+++ 3.Codigo/backend-test/server/server.js
@@ -5,14 +5,6 @@ var boot = require('loopback-boot');
 
 var app = loopback();
 
-app.use(function (req, res, next) {
-
-    console.log(req.headers)
-
-    next();
-
-})
-
 boot(app, __dirname, function (err) {
 
     if (err)
@@ -23,7 +15,6 @@ boot(app, __dirname, function (err) {
         if (err)
             throw err
 
-        app.emit("started")
         console.log(`Listening ...`);
 
     })
